12414d126e8d5c97e7cb6a609279eb3e50dd823f
dev_one <micool1030@gmail.com>
2023-12-21 18:14:48 +0900

    [feat] v1 - 인터페이스와 구현 클래스 - 스프링 빈으로 수동 등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록(수동등록)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //이렇게 하는 경우 지정 폴더와 하위 폴더만 component scan 대상(스프링이 뜰때 해당 패키지만 등록) : 버전 별로 config 스캔하려고 지정
 public class ProxyApplication {
 
 	public static void main(String[] args) {
