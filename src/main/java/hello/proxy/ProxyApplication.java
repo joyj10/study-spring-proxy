ed312507cb58d7ee4921fe8007ebea48a389424e
dev_one <micool1030@gmail.com>
2023-12-23 18:51:19 +0900

    [feat] 인터페이스 기반 프록시 적용 v1

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,15 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록(수동등록)
+//@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록(수동등록)
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //이렇게 하는 경우 지정 폴더와 하위 폴더만 component scan 대상(스프링이 뜰때 해당 패키지만 등록) : 버전 별로 config 스캔하려고 지정
 public class ProxyApplication {
 
@@ -14,4 +17,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
