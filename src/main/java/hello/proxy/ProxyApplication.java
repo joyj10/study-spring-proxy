28ce004f0d63bbba3f0d2fc90e49fe642a05a746
dev_one <micool1030@gmail.com>
2023-12-29 16:11:47 +0900

    [feat] JDK 동적 프록시 적용(basic, filter)

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록(수동등록)
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //이렇게 하는 경우 지정 폴더와 하위 폴더만 component scan 대상(스프링이 뜰때 해당 패키지만 등록) : 버전 별로 config 스캔하려고 지정
 public class ProxyApplication {
 
